5fb67b8fa16c8c40cc2316633c5174f5878731a2
Matheus <m258602@dac.unicamp.br>
2026-02-04 17:11:09 +0000

    header and footer formatting & moved to _layout

--- my-expo-app/app/(tabs)/_layout.tsx
+++ my-expo-app/app/(tabs)/_layout.tsx
@@ -1,44 +1,69 @@
 import { Pressable, View, Text, StyleSheet} from 'react-native';
 import '../../global.css';
-import { Stack } from 'expo-router';
-import { FontAwesome } from '@expo/vector-icons';
-import { Link } from 'expo-router';
-import { StackScreen } from 'react-native-screens';
 import React, {useState} from 'react';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { Stack, Link } from 'expo-router';
+import {FontAwesome} from '@expo/vector-icons';
+import { Slot } from 'expo-router';
 
-// nome do usuario obtido atraves do login
-const user = 'Absence'
-
-export default function Layout() {
-  return <Stack 
-    screenOptions={{
-        headerStyle: {
-          backgroundColor: '#ffffffff',
-        },
-        headerTintColor: '#000000ff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-        headerRight: () => (
-          <View style={{flexDirection: 'row-reverse'}}>
-          <Link href="/settings">
-            <Pressable>
-              <FontAwesome name="cog" size={25} color="black" />
-            </Pressable>
-          </Link>
-          <Link href="/">
-            <Pressable>
-              <FontAwesome name="home" size={25} color="black" style={{ marginRight: 30 }}/>
+const Header = () => {
+  const user = 'Absence';
+  return (
+    <View className="flex-row justify-between items-center bg-white p-4 rounded-t-3xl shadow-lg w-full">
+       <Text style={styles.titleText}>Hi, {user}!</Text>
+       <View className="flex-row items-center gap-2" style={{flexDirection: 'row-reverse'}}>
+           <Link href="/settings" asChild>
+             <Pressable hitSlop={10}>
+               <FontAwesome name="cog" size={25} color="black" />
+             </Pressable>
+           </Link>
+           <Link href="/" asChild>
+             <Pressable hitSlop={10}>
+               <FontAwesome name="home" size={25} color="black" style={{ marginRight: 30 }}/>
             </Pressable> 
           </Link>
-          </View>
-        ),
-        headerLeft: () => (
-          <Text style={styles.titleText}>Hi, {user}!</Text>
-        ),
-      }}
-    />;
+        </View>
+    </View>
+  )
+}
+
+// BACK BUTTON FUNCTIONALLITY ADD !?!?!?!?!?
+// #4A148C = COR BONITA; SETA = 'reply' = or 'arrow-left' and 'chevron-left'
+
+const Footer = () => {
+  return (
+   <View style={{flex:1, backgroundColor: '#D9D9D9'}} className="flex-row justify-around items-center bg-gray p-4 rounded-t-3xl shadow-lg absolute bottom-0 w-full">
+        <Stack.Screen options={{ title: '' }} />
+          <Link href="/disciplinas" asChild>
+            <Pressable className="items-center">
+              <FontAwesome name="book" size={25} color="#000000" /> 
+              <Text className="text-xs font-bold text-gray-800">Disciplines</Text>
+            </Pressable>
+          </Link>
+          <Link href="/notifications" asChild>
+            <Pressable className="items-center">
+              <FontAwesome name="bell" size={25} color="#000000" />
+              <Text className="text-xs font-bold text-gray-800">Notifications</Text>
+            </Pressable>
+          </Link>
+          <Link href="/profile" asChild>
+            <Pressable className="items-center">
+              <FontAwesome name="address-card" size={25} color="#000000" />
+              <Text className="text-xs font-bold text-gray-800">Profile</Text>
+            </Pressable>
+          </Link>
+      </View>
+  )
+}
+
+export default function Layout() {
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor: '#f3f3f3'}} edges={['top', 'left', 'right']}>
+      <Header />
+      <Slot />
+      <Footer />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
--- my-expo-app/app/(tabs)/index.tsx
+++ my-expo-app/app/(tabs)/index.tsx
@@ -10,27 +10,7 @@ const data = {
 
 export default function Home() {
   return (
-  <View className="flex-row justify-around items-center bg-white p-4 rounded-t-3xl shadow-lg absolute bottom-0 w-full">
-  <Stack.Screen options={{ title: '' }} />
-  <Link href="/disciplinas" asChild>
-    <Pressable className="items-center">
-      <FontAwesome name="book" size={25} color="#4A148C" /> 
-      <Text className="text-xs font-bold text-gray-800">Disciplines</Text>
-    </Pressable>
-  </Link>
-  <Link href="/notifications" asChild>
-    <Pressable className="items-center">
-    <FontAwesome name="bell" size={25} color="#4A148C" />
-    <Text className="text-xs font-bold text-gray-800">Notifications</Text>
-  </Pressable>
-  </Link>
-  <Link href="/profile" asChild>
-    <Pressable className="items-center">
-    <FontAwesome name="address-card" size={25} color="#4A148C" />
-    <Text className="text-xs font-bold text-gray-800">Profile</Text>
-  </Pressable>
-  </Link>
-  </View>
+    <View className='bg-black'></View>
   );
 }
 
