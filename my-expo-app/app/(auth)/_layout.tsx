10849fdfc8ca39a8477b96a44db0cd1aa3030c95
Matheus <m258602@dac.unicamp.br>
2026-02-09 22:43:28 +0000

    login clerk structure add

---
+++ my-expo-app/app/(auth)/_layout.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, ImageBackground} from 'react-native';
+import '../../global.css';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import { Slot } from 'expo-router';
+
+
+const image = {url: 'my-expo-app/assets'}
+
+export default function Layout() {
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor: '#f3f3f3'}} edges={['top', 'left', 'right']}>
+      <ImageBackground source={require('../../assets/Background.png')} resizeMode="cover" style={styles.image}>
+        <Slot />
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
