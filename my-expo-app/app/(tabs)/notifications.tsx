ce13c3463dfe2a031d30cf5b9731bde955c45b1b
Matheus <m258602@dac.unicamp.br>
2026-02-11 01:18:25 +0000

    notifications dinamic logic

--- my-expo-app/app/(tabs)/notifications.tsx
+++ my-expo-app/app/(tabs)/notifications.tsx
@@ -4,33 +4,93 @@ import { ScrollView, StyleSheet, View, Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { FontAwesome } from '@expo/vector-icons';
 
-// Precisa da hora em que a not fora emitida e o user
-const data = {
-
-}
-
 export default function Details() {
+  // Precisa do horario de emissao, materia cancelada, user and notification_type
+  type notType = 'late' | 'cancel' | 'skip' | 'exam';
+  type notData = {
+    time: string, // horario de envio da notificacao no servidor
+    horario: string, // horario aula referenciado
+    discipline: string, // disciplina envolvida
+    user: string, // usuario responsavael/ligado
+    type: notType, // tipo de notificacao
+    day: string, // dia referente a notificacao
+  }
+  const data : notData[] =[
+    {
+      time: "00:32",
+      horario: "14:00-16:00",
+      discipline: "MC211",
+      user: "KGM",
+      type: "skip",
+      day: "Mon", // Mudar para ser data, nao dia da semana random kkkkkk
+    },
+    {
+      time: "14:34",
+      horario: "18:00-19:00",
+      discipline: "F259",
+      user: "Y",
+      type: "cancel",
+      day: "Tue"
+    },
+    {
+      time: "08:01",
+      horario: "19:00-21:00",
+      discipline: "MA323",
+      user: "VHAM",
+      type: "late",
+      day: "Fri",
+    },
+    {
+      time: "18:32",
+      horario: "21:00-23:00",
+      discipline: "MC358",
+      user: "BEDUNADO",
+      type: "exam",
+      day: "Wed",
+    },
+  ];
+  const persoMessages = {
+    late: "A sua aula de {discipline} fora remarcada ou teve seu horario padrao alterado para {day} as {horario}",
+    cancel: "A sua aula de {discipline} de {day} as {horario} fora cancelada",
+    skip: "O usuario {user} nao ira comparecer na aula de {discipline} as {horario} de {day}",
+    exam: "A prova de {discipline} de {day} as {horario} se aproxima!",
+  };
   // notification_type: "equivalent_icon_name"
-  const relationship = {
+  const iconRelationship = {
     late: "map-signs", cancel: "window-close", 
     skip: "user-o", exam: "calendar-check-o" 
+  } as const;
+  const titleRelationship = {
+    late: "Late/Rescheduled Class", cancel: "Class Canceled", 
+    skip: "Skip Class", exam: "Exam`s Close" 
+  } as const;
+  const formatMessage = (not: notData) => {
+      let message = persoMessages[not.type]; // erro de type
+      message = message.replace("{discipline}", not.discipline);
+      message = message.replace("{day}", not.day);
+      message = message.replace("{horario}", not.horario);
+      message = message.replace("{user}", not.user);
+      return message;
   }
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={styles.scrollContent}>
-          <View style={styles.notificationBlock}>
-            <FontAwesome style={styles.icon} name="map-signs" size={40} color='black'></FontAwesome>
-            <View style={styles.textBlock}>
-              <Text style={styles.title}>Late Class</Text>
-              <Text style={styles.body}>A sua aula fora remarcada ou teve seu horario
-              padrao alterado para o dia 'day' as 'horario novo'</Text>
+          {data.map(not => (
+            <View style={styles.notificationBlock}>
+              <FontAwesome style={styles.icon} name={iconRelationship[not.type]} size={40} color="#000000"></FontAwesome>
+              <View style={styles.textBlock}>
+                <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+                  <Text style={styles.title}>{titleRelationship[not.type]}</Text>
+                  <Text style={styles.timeElement}>{not.time}</Text>
+                </View>
+                {/* FORMATACAO INCORRETA (PODE MELHORAR) */}
+                <Text style={styles.body}>{formatMessage(not)}</Text>
+              </View>
+                {/* ADICIONAR USUARIO QUE CRIOU A NOT NA MENSAGEM??? */}
+                {/* <Text style={styles.detailsElement}>by kgm</Text> */}
             </View>
-            <View style={styles.details}>
-              <Text style={styles.detailsElement}>2h ago</Text>
-              {/* <Text style={styles.detailsElement}>by kgm</Text> */}
-            </View>
-          </View>
+          ))}
         </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -58,6 +118,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#D9D9D9',
     opacity: .5,
     flexDirection: "row",
+    marginVertical: 5
   },
   icon: {
     marginLeft: 10,
@@ -66,6 +127,8 @@ const styles = StyleSheet.create({
   textBlock: {
     padding: 10,
     flex:3,
+    justifyContent: "space-between",
+    alignContent: "flex-end"
   },
   title: {
     fontWeight: "bold",
@@ -76,14 +139,13 @@ const styles = StyleSheet.create({
     alignContent: "flex-start",
     flexShrink: 1,
   },
-  details: {
-    flex:1,
-    marginVertical: 10,
-    justifyContent: "space-between",
-    alignContent: "flex-end"
-  },
-  detailsElement: {
-    alignSelf: "flex-end",
-    marginRight: 10
+  // details: {
+  //   flex:1,
+  //   marginVertical: 10,
+  //   justifyContent: "space-between",
+  //   alignContent: "flex-end"
+  // },
+  timeElement: {
+    opacity: .6
   }
 });
