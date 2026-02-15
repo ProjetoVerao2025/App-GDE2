f06bd291d44b84db0d2e0d409e44596c649b1576
Matheus <m258602@dac.unicamp.br>
2026-02-15 19:57:11 +0000

    Not found page add

--- my-expo-app/app/+not-found.tsx
+++ my-expo-app/app/+not-found.tsx
@@ -1,27 +1,66 @@
-import { Link, Stack } from 'expo-router';
-
-import { Text, View, Image } from 'react-native';
-
-import { Container } from '@/components/Container';
+import { Link} from 'expo-router';
+import { Text, View, Image, StyleSheet, ScrollView } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function NotFoundScreen() {
   return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <Container>
-        <Text className={styles.title}>{"This screen doesn't exist."}</Text>
-        <Image source={require("../assets/sry.png")}/>
-        <Link href="/" className={styles.link}>
-          <Text className={styles.linkText}>Go to home screen!</Text>
-        </Link>
-      </Container>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+          <View style={styles.container}>
+            <Text style={styles.title}>{"This screen doesn't exist."}</Text>
+            <Image style={styles.image} source={require("../assets/sry.png")}/>
+            <View style={styles.row}>
+              <Link href="/" asChild>
+                <Text style={styles.link}>Go to home screen!</Text>
+              </Link>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = {
-  container: `flex flex-1 bg-white`,
-  title: `text-xl font-bold`,
-  link: `mt-4 pt-4`,
-  linkText: `text-base text-[#2e78b7]`,
-};
+const styles = StyleSheet.create({
+  // container: `flex flex-1 bg-white`,
+  // title: `text-xl font-bold`,
+  // link: `mt-4 pt-4`,
+  // linkText: `text-base text-[#2e78b7]`,
+  container: {
+    flex: 1,
+    flexGrow: 1,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignContent: "center"
+  },
+  safeArea: {
+    flex:1
+  },
+  scrollContent: {
+    paddingBottom: 40, 
+    paddingVertical: 60, 
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+    alignContent: "center"
+  },
+  image: {
+    padding: 10,
+    width: "100%",
+    maxWidth: 420
+  },
+  link: {
+    fontWeight: 'bold',
+    color: "#54a1e1ff"
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    marginVertical: 10,
+    justifyContent: "center",
+  }
+});
